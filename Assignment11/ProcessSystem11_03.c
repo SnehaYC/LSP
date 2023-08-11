39f45b1f062da879f50e76597d9656d1398e1d42
Sneha Y. Choudhari <62131585+SnehaYC@users.noreply.github.com>
2023-08-11 23:28:09 +0530

    Add files via upload

--- Assignment11/ProcessSystem11_03.c
+++ Assignment11/ProcessSystem11_03.c
@@ -12,9 +12,36 @@
 #include<pthread.h>
 
 
-int main()
+pthread_t tid[2];
+int iCnt;
+  
+void* threadProcess(void* arg)
 {
-	
-	
-	return 0;
+    unsigned long i = 0;
+    iCnt = iCnt + 1;
+    printf("\n process %d has started\n", iCnt);
+  
+    for (i = 0; i < (0xFFFFFFFF); i++)   //Hexadecimal number 4294967295
+        ;
+    printf("\n process %d has finished\n", iCnt);
+  
+    return NULL;
+}
+  
+int main(void)
+{
+    int i = 0;
+    int error;
+  
+    while (i < 2) {
+        error = pthread_create(&(tid[i]), NULL, &threadProcess, NULL);
+        if (error != 0)
+            printf("\nThread can't be created : [%s]", strerror(error));
+        i++;
+    }
+  
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+  
+    return 0;
 }
--- Assignment11/ProcessSystem11_04.c
+++ Assignment11/ProcessSystem11_04.c
@@ -1,6 +1,6 @@
 /*
  *---------------------------
- * A program which creates two threads and by using appropriate synchronization technique avoid the scheduling probolem.
+ * A program which creates two threads and by using appropriate synchronization technique avoid the scheduling problem.
  * Author: Sneha YC
  *---------------------------
  */
@@ -11,9 +11,48 @@
 #include<fcntl.h>
 #include<pthread.h>
 
-int main()
+pthread_t tid[2];
+int counter;
+pthread_mutex_t lock;
+  
+void* threadProcess(void* arg)
 {
-	
-	
-	return 0;
+    pthread_mutex_lock(&lock);
+  
+    unsigned long i = 0;
+    counter += 1;
+    printf("Process %d has started\n", counter);
+  
+    for (i = 0; i < (0xFFFFFFFF); i++);
+        
+  
+    printf("Process %d has finished\n", counter);
+  
+    pthread_mutex_unlock(&lock);
+  
+    return NULL;
+}
+  
+int main(void)
+{
+    int i = 0;
+    int error;
+  
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("Mutex init has failed\n");
+        return 1;
+    }
+  
+    while (i < 2) {
+        error = pthread_create(&(tid[i]), NULL,  &threadProcess, NULL);
+        if (error != 0)
+            printf("Thread can't be created :[%s]", strerror(error));
+        i++;
+    }
+  
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_mutex_destroy(&lock);
+  
+    return 0;
 }
